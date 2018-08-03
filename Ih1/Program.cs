e970df6b487c384afc9d44d0b3e3cf58564c22ad
Adrian Sims <adriansims62781@gmail.com>
2018-08-03 16:05:37 +0200

    Add project files.

---
+++ Ih1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ih1
+{
+    class A
+    {
+        protected A()
+        {
+            Console.WriteLine("Protected constructor A");
+        }
+
+        public void F()
+        {
+            Console.WriteLine("F in class A");
+        }
+    }
+
+    class B : A
+    {
+        public B()
+        {
+            Console.WriteLine("Protected constructor B");
+        }
+
+        protected new virtual void F()
+        {
+            Console.WriteLine("F in class B");
+        }
+    }
+
+    class C : B
+    {
+        public C()
+        {
+            Console.WriteLine("Protected constructor B");
+        }
+        protected override void F()
+        {
+            Console.WriteLine("F in class C");
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                (new A()).F();
+                ((A)new B()).F();
+                
+                ((A)new B()).F();
+            }
+            catch (Exception ex)
+            {
+                var codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                var progname = Path.GetFileNameWithoutExtension(codeBase);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+
+        }
+    }
+}
---
+++ gitmehappy.cmd
@@ -0,0 +1,8 @@
+rem echo off
+git init
+git add Ih1/Program.cs
+git add Ih1/App.config
+git add Ih1/Properties/AssemblyInfo.cs
+git add Ih1/Ih1.csproj
+git add Ih1.sln
+git add .gitignore
